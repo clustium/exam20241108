c094283347e6b2a81ba107b7131f42e0baea7572
junohm410 <junohm410@gmail.com>
2024-11-08 15:58:09 +0900

    引数の中に10進数の数値としてパースでいない値が含まれている場合はエラーにするよう改修

--- sumValues.js
+++ sumValues.js
@@ -1,6 +1,16 @@
 function sumValues(...args) {
-  const sum = args.reduce((sum, currentValue) => (sum += currentValue), 0);
+  const convertedArgs = args.map((arg) => {
+    const convertedArg = parseInt(arg, 10);
+    if (isNaN(convertedArg)) {
+      throw "引数に10進数としてパースできない値が含まれています";
+    }
+
+    return convertedArg;
+  });
+
+  const sum = convertedArgs.reduce(
+    (sum, currentValue) => (sum += currentValue),
+    0
+  );
   console.log(sum);
 }
-
-sumValues(1, 2, 5);
